8a93a4da1053f0e4b55a250d8181164c531fab72
dukewellington61 <dukewellington61@googlemail.com>
2021-02-19 16:20:10 +0100

    2nd commit

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -76,7 +76,7 @@ export const App = () => {
         <div className="main">
           {user ? (
             <Fragment>
-              <TaskForm user={user} />
+              <TaskForm />
 
               <div className="filter">
                 <button onClick={() => setHideCompleted(!hideCompleted)}>
